e38cb9946696e7e8dd08ee8433c215259c0b85de
Benjamin <benjamin.d42@gmail.com>
2016-02-26 21:51:50 +0100

    Add tests

--- src/Alg/Msg2fs/Msg2fs.php
+++ src/Alg/Msg2fs/Msg2fs.php
@@ -7,7 +7,34 @@ class Msg2fs {
     /**
      * @var string
      */
-    private static $spoolDir = '/var/spool/msg2fs/';
+    private $spoolDir = '/var/spool/msg2fs/';
+
+    /**
+     * @var string
+     */
+    private $exchange;
+
+    /**
+     * @var string
+     */
+    private $routingKey;
+
+    /**
+     * @var string
+     */
+    private $outFile;
+
+    /**
+     * Msg2fs constructor.
+     *
+     * @return self
+     */
+    public function __construct() {
+        if(!$this->spoolDir = getenv('SPOOLDIR'))
+            $this->spoolDir = '/var/spool/msg2fs/';
+
+        return $this;
+    }
 
     /**
      * Save message to filesystem.
@@ -16,57 +43,46 @@ class Msg2fs {
      * @param string $exchange
      * @param string $routingKey
      */
-    public static function save(array $msg, $exchange, $routingKey) {
-        $localId = self::formatLocalId($exchange, $routingKey);
-        $outFile = self::$spoolDir . $localId;
+    public function save(array $msg, $exchange, $routingKey) {
+        $this->msg = $msg;
+        $this->exchange = $exchange;
+        $this->routingKey = $routingKey;
 
-        $msg = self::improveMsgHead($msg, $outFile);
+        $localId = $this->formatLocalId();
+        $this->outFile = $this->spoolDir . $localId;
 
-        self::write($outFile, json_encode($msg));
+        $this->improveMsgHead();
+
+        $this->flush();
     }
 
     /**
      * Return localId well formated.
      *
-     * @param string $exchange
-     * @param string $routingKey
-     *
      * @return string
      */
-    private static function formatLocalId($exchange, $routingKey) {
-        return microtime(true) . "." . substr(md5(rand()), 0, 8) . "@" . $routingKey . "@" . $exchange . ".v1";
+    private function formatLocalId() {
+        return microtime(true) . "." . substr(md5(rand()), 0, 8) . "@" . $this->routingKey . "@" . $this->exchange . ".v1";
     }
 
     /**
      * Add system vars to message head.
-     *
-     * @param array $msg
-     * @param string $outFile
-     *
-     * @return array
      */
-    private static function improveMsgHead(array $msg, $outFile) {
-        if (false == array_key_exists("head", $msg))
-            $msg['head'] = array();
+    private function improveMsgHead() {
+        if (false == array_key_exists("head", $this->msg))
+            $this->msg['head'] = array();
 
-        $msg['head']['x-msg2fs-fid'] = $outFile;
-        $msg['head']['x-msg2fs-hn'] = gethostname();
-        $msg['head']['x-msg2fs-dt'] = (new \DateTime())->format(DATE_ISO8601);
-
-        return $msg;
+        $this->msg['head']['x-msg2fs-fid'] = $this->outFile;
+        $this->msg['head']['x-msg2fs-hn'] = gethostname();
+        $this->msg['head']['x-msg2fs-dt'] = (new \DateTime())->format(DATE_ISO8601);
     }
 
     /**
      * Write message to filesystem
-     *
-     * @param string $outFile
-     * @param string $msg
-     *
-     * @throws \Exception
      */
-    private static function write($outFile, $msg) {
-        if (false === file_put_contents($outFile, $msg));
-            throw new \Exception(sprintf('File %s not writable', $outFile));
+    private function flush() {
+        $file = new \SplFileObject($this->outFile, 'w');
+        $file->fwrite(json_encode($this->msg));
     }
 
 }
